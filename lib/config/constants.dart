eabaf874b4d9cab2c8f2d93c6fd23bd95b024a33
sanjay.sah <sanjay.sah@intellore.com>
2020-09-13 17:57:17 +0530

    Added next button at bottom and given background color

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_registration/config/size_config.dart';
 
+const kPrimaryColor = Colors.blue;
+const kPrimaryLightColor = Color(0x992196F3);
+const kBackgroundColor = Colors.indigo;
+
+
 const kCurrentRegistrationStepColor = Colors.green;
 const kDefaultRegistrationStepColor = Colors.white;
 const kTextColor = Color(0xFF757575);
--- lib/screens/registration/registration_main_screen.dart
+++ lib/screens/registration/registration_main_screen.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_registration/config/constants.dart';
 import 'package:flutter_registration/config/size_config.dart';
+import 'package:flutter_registration/screens/registration/components/registration_step_one.dart';
 import 'package:flutter_registration/screens/registration/components/registration_steps.dart';
+import 'package:flutter_registration/widgets/default_button.dart';
 
-class RegistrationMainScreen extends StatelessWidget {
+class RegistrationMainScreen extends StatefulWidget {
   static String routeName = "/registration";
+
+  @override
+  State<StatefulWidget> createState() => _RegistrationMainScreenState();
+}
+
+class _RegistrationMainScreenState extends State<RegistrationMainScreen> {
+  int _currentStep = 1;
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: RegistrationSteps(),
+      backgroundColor: kBackgroundColor,
+      body: _body,
     );
   }
+
+  Widget get _body => Container(
+    margin: EdgeInsets.all(16),
+    child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            RegistrationSteps(),
+            _currentRegistrationStepPage,
+            _nextButton
+          ],
+        ),
+  );
+
+  Widget get _currentRegistrationStepPage {
+    switch (_currentStep) {
+      case 1:
+        return RegistrationStepOne();
+      default:
+        return Container();
+    }
+  }
+
+  Widget get _nextButton => DefaultButton(
+    text: 'NEXT',
+    press: () {
+    },
+  );
 }
